f7fce6d242cbddd77d2d5708d610bc385fa76882
roboticsslovenia <81637815+roboticsslovenia@users.noreply.github.com>
2022-10-07 12:48:30 +0200

    Update test_list.py

--- test_list.py
+++ test_list.py
@@ -1,14 +1,24 @@
 def rec_sum_ends(lst):
-  if len(lst) <= 2:
-    return lst
-  midpoint = ((len(lst)) // 2)
-  #print(f"midpoint {midpoint}")
-  front_half = lst[:midpoint]
-  reversed_back_half = lst[:-midpoint-1:-1]   
-  combined = [sum(el) for el in zip(front_half, reversed_back_half)]   
-  combined.append(lst[len(lst)//2])
-  print(combined)
+    if len(lst) <= 2:
+        return lst
+    if len(lst) % 2 == 0:
 
-  return rec_sum_ends(combined)
+        front_half = lst[:len(lst)//2]
+        reversed_back_half = lst[:len(lst)//2-1:-1]
+        combined = [sum(el) for el in zip(front_half, reversed_back_half)]   
+        
+
+    else:
+
+        midpoint = ((len(lst)) // 2)
+        #print(f"midpoint {midpoint}")
+        front_half = lst[:midpoint]
+        reversed_back_half = lst[:-midpoint-1:-1]   
+        combined = [sum(el) for el in zip(front_half, reversed_back_half)]   
+        combined.append(lst[len(lst)//2])
+
+    print(combined)
     
-print(rec_sum_ends([1, 2, 1, 2, 1, 3, 1, 1, 1]))
+    return rec_sum_ends(combined)
+
+print(rec_sum_ends([1, 2, 1, 2, 1, 1, 1, 1]))
